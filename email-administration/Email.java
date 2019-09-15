75d4c8abb65c0c09fe66ab6ec5860270deb9af1c
Sook-Hee Evans <evanssookhee@gmail.com>
2019-09-15 13:25:54 -0400

    Add files via upload

---
+++ email-administration/Email.java
@@ -0,0 +1,21 @@
+package emailapp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String alternateEmail;
+	private int mailboxCapacity;
+
+	// Constructor: takes first and last name, dept
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: " + thisfirstName + " " + this.lastName);
+	}
+	// Generate random password
+	// Set mailbox capacity
+	// Set alternate email
+
+}
